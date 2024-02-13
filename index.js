8ca236d89ec4e81e9897972145125b2908fd0f0f
Belvin-Benny <belvin.benny@harvestinfosoft.com>
2024-02-13 19:59:58 +0530

    fixes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
     res.header(
       "Access-Control-Allow-Origin",
-      "https://snive-frontend.vercel.app/"
+      "https://snive-frontend.vercel.app"
     );
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
