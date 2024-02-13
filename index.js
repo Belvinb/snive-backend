de5f1d6941883f37499529d3fab919efb4b49bfd
Belvin-Benny <belvin.benny@harvestinfosoft.com>
2024-02-13 18:11:15 +0530

    mongo issue fixed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const userRoutes = require("./Routes/userRoutes")
 const noteRoutes = require("./Routes/noteRoutes")
 const app = express()
 app.use(cors());
-connectDB()
+
 
 app.use(express.json())
 app.use(cookieParser());
@@ -15,6 +15,9 @@ app.use(cookieParser());
 app.use("/api/user",userRoutes)
 app.use("/api/note",noteRoutes)
 
-app.listen(5000,()=>{
-    console.log("connected to backend")
+connectDB().then(()=>{
+
+    app.listen(5000,()=>{
+        console.log("connected to backend")
+    })
 })
