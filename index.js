0441fe5b5e75a10ab6adb5c2571dd9ff003a3aea
Belvin-Benny <belvin.benny@harvestinfosoft.com>
2024-02-13 18:25:48 +0530

    cors issue

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.use(cors());
 app.use(express.json())
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "https://vercel.com/belvinb/snive-frontend"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+
 app.use("/api/user",userRoutes)
 app.use("/api/note",noteRoutes)
 
